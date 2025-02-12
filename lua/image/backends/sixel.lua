1556f9a54d32b8adafdcacaaa288d38e302f9639
Jared Yang <nociszy@gmail.com>
2025-02-11 20:02:11 -0800

    Update sixel.lua

--- lua/image/backends/sixel.lua
+++ lua/image/backends/sixel.lua
@@ -88,7 +88,7 @@ backend.clear = function(image_id, shallow)
     local x = image.geometry.x
     local y = image.geometry.y
     -- geometry unable to provide correct x y coords here unless we reassign it at rendering stage 
-    -- vim.notify("x:"..x .." y:".. y,vim.log.levels.WARN)
+    vim.notify("x:"..x .." y:".. y,vim.log.levels.WARN)
     vim.defer_fn(function()
       backend.stdout:write(string.format("\27[s\27[%d;%dHtest\27[u", y + 1, x + 1))
     end, 50)
