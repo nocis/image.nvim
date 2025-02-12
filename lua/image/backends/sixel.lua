3cd775c6a293056d99ca01ad9fcc231ab504a8f6
Jared Yang <nociszy@gmail.com>
2025-02-11 20:06:24 -0800

    Update sixel.lua

--- lua/image/backends/sixel.lua
+++ lua/image/backends/sixel.lua
@@ -39,7 +39,7 @@ backend.render = function(image, x, y, width, height)
                   string.format("img2sixel -w %d -h %d %s", height*aspect_ratio, height, image.cropped_path),
                   vim.log.levels.WARN
                 )
-  vim.notify("x:"..x .." y:".. y,vim.log.levels.WARN)
+  vim.notify("render x:"..x .." y:".. y,vim.log.levels.WARN)
   if image.is_rendered ~= true then
     _render_sixel_str(sixel_str, x, y)
     image.is_rendered = true
@@ -88,7 +88,7 @@ backend.clear = function(image_id, shallow)
     local x = image.geometry.x
     local y = image.geometry.y
     -- geometry unable to provide correct x y coords here unless we reassign it at rendering stage 
-    vim.notify("x:"..x .." y:".. y,vim.log.levels.WARN)
+    vim.notify("clear x:"..x .." y:".. y,vim.log.levels.WARN)
     vim.defer_fn(function()
       backend.stdout:write(string.format("\27[s\27[%d;%dHtest\27[u", y + 1, x + 1))
     end, 50)
@@ -107,7 +107,7 @@ backend.clear = function(image_id, shallow)
                 )
       local x = image.geometry.x
       local y = image.geometry.y
-      vim.notify("x:"..x .." y:".. y,vim.log.levels.WARN)
+      vim.notify("clear x:"..x .." y:".. y,vim.log.levels.WARN)
       vim.defer_fn(function()
         backend.stdout:write(string.format("\27[s\27[%d;%dHtest\27[u", y + 1, x + 1))
       end, 50)
